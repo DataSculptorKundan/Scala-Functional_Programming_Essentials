78694fa52ed8cbfb07b91f1456adb71ae363e01a
Kundan Kumar <kkumar.cpr@outlook.com>
2023-12-19 15:47:27 +0530

    Added Expressions class

--- src/main/scala/lecture/the_absolute_scala_basics/Expressions.scala
+++ src/main/scala/lecture/the_absolute_scala_basics/Expressions.scala
@@ -1,5 +1,26 @@
 package lecture.the_absolute_scala_basics
+object Expressions extends App {
 
-object Expressions {
+  val x = 25 + 25  //Expression
+  println(x)
+
+  println(2 + 4 * 10)
+  println(10 == x) // False
+  println(!(1 == x)) //True
+
+  var aVariable = 20
+  aVariable += 10
+  println(aVariable)
+
+  // Instruction (DO) vs Expression (VALUE)
+
 
 }
+
+/*
+Add(+), Subtract(-), Multiply(*), Divide(/), Bitwise(&), BitwiseOR(|), BitwiseExclusiveOR(^)
+BitwiseLeftShift(<<), BitwiseRightShift(>>)
+RightShiftWithZeroExtension(>>>)
+Equality(==), NoneEquality(!=)
+Only work with variables (+= -= *=)
+ */
