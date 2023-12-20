353a80c1f0f8418a15a14f9f26f4109929210139
Kundan Kumar <kkumar.cpr@outlook.com>
2023-12-20 23:41:21 +0530

    Updated

--- src/main/scala/lecture/the_absolute_scala_basics/Functions.scala
+++ src/main/scala/lecture/the_absolute_scala_basics/Functions.scala
@@ -10,10 +10,20 @@ object Functions extends App {
   println(aParameterlessFunction())
   //println(aParameterlessFunction)
 
+  // When we need to write the loop, then use Recursion instead of loop.
   def aRepeatFunction(aString: String, n: Int): String = {
     if (n == 1) aString
     else aString + aRepeatFunction(aString, n-1)
   }
-  println(aRepeatFunction("London",3))
+  println(aRepeatFunction("London",3)) // Alternative ways of Loop
+
+  def aFunctionWithSideEffects(aString: String): Unit = println(aString)
+
+  def aBigFunction(n: Int): Int = {
+    def aSmallFunction(a: Int, b: Int): Int = a + b
+    aSmallFunction(n, n-1)
+  }
+  println(aBigFunction(8))
+
 
 }
